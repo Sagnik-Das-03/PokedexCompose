e4dd50dafce14297c1242250ab95c66daabb847c
Sagnik-Das-03 <sagnik07das@gmail.com>
2023-04-23 12:02:02 +0530

    retrofit and api interface set up for network calls

---
+++ app/src/main/java/com/example/pokedexCompose/data/remote/responses/IconsX.kt
@@ -0,0 +1,6 @@
+package com.example.pokedexCompose.data.remote.responses
+
+data class IconsX(
+    val front_default: String,
+    val front_female: String
+)
---
+++ app/src/main/java/com/example/pokedexCompose/data/remote/responses/VersionDetail.kt
@@ -0,0 +1,6 @@
+package com.example.pokedexCompose.data.remote.responses
+
+data class VersionDetail(
+    val rarity: Int,
+    val version: Version
+)
